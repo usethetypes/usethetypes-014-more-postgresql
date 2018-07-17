9620b612cae3789981a557d74d1bbab38f108b65
Richard Cook <richard@usethetypes.com>
2018-07-16 17:19:36 -0700

    Step 7: Matching paths

--- src/Main.hs
+++ src/Main.hs
@@ -3,10 +3,11 @@
 module Main (main) where
 
 import Control.Applicative ((<|>))
-import Snap.Core (ifTop, writeText)
+import Snap.Core (ifTop, route, writeText)
 import Snap.Http.Server (quickHttpServe)
 
 main :: IO ()
 main = quickHttpServe $
     ifTop (writeText "Hello world")
+    <|> route [ ("/ping", writeText "Ping") ]
     <|> writeText "Bad path"

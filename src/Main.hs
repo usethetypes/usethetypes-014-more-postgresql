0cd83c11dbcadd359830c3009c16dfc1fc7ffe20
Richard Cook <richard@usethetypes.com>
2018-12-01 14:16:10 -0800

    Serve /static route from app

--- src/Main.hs
+++ src/Main.hs
@@ -5,8 +5,11 @@ module Main (main) where
 import Control.Applicative ((<|>))
 import Snap.Core (ifTop, route, writeText)
 import Snap.Http.Server (quickHttpServe)
-import Snap.Util.FileServe (serveFile)
+import Snap.Util.FileServe (serveDirectory, serveFile)
 
 main :: IO ()
 main = quickHttpServe $
     ifTop (serveFile "views/index.html")
+    <|> route
+        [ ("/static", serveDirectory "static")
+        ]

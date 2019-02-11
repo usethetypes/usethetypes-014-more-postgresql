ab360fe3a385299756901ecd19e7455e48d96ec6
Richard Cook <richard@usethetypes.com>
2019-02-10 21:39:11 -0800

    Implement showUsers

--- src/Main.hs
+++ src/Main.hs
@@ -4,31 +4,45 @@ module Main (main) where
 
 import Control.Applicative ((<|>))
 import Control.Monad.IO.Class (MonadIO(..))
+import Data.ByteString (ByteString)
+import Data.Maybe (fromMaybe)
 import Data.Text (Text)
+import Database.PostgreSQL.Simple
+    ( ConnectInfo
+    , Connection
+    , Only(..)
+    , query_
+    )
 import Snap.Core
-        ( Method(..)
-        , MonadSnap
-        , ifTop
-        , method
-        , route
-        )
+    ( Method(..)
+    , MonadSnap
+    , ifTop
+    , method
+    , route
+    )
 import Snap.Http.Server (httpServe, setPort)
 import Snap.Util.FileServe (serveDirectory, serveFile)
 import System.Environment (getEnvironment, lookupEnv)
 import Text.Ginger ((~>), dict)
 
+import App.DBUtil
 import App.Template
 
 main :: IO ()
 main = do
     mbPort <- lookupEnv "PORT"
-    let port = maybe 8000 read mbPort
-        config = setPort port mempty
+    let !port = maybe 8000 read mbPort
+    mbDBUrl <- lookupEnv "DATABASE_URL"
+    let dbUrl = fromMaybe (error "DATABASE_URL not set") mbDBUrl
+        !connInfo = fromMaybe (error "DATABASE_URL is not a valid PostgreSQL URL") (parseDBUrl dbUrl)
+
+    let config = setPort port mempty
     httpServe config $
         ifTop (serveFile "views/index.html")
         <|> route
-            [ ("/info", method GET showInfo)
-            , ("/static", serveDirectory "static")
+            [ ("/info" :: ByteString, method GET showInfo)
+            , ("/users" :: ByteString, method GET (showUsers connInfo))
+            , ("/static" :: ByteString, serveDirectory "static")
             ]
 
 showInfo :: MonadSnap m => m ()
@@ -38,3 +52,13 @@ showInfo = do
         [ ("title" ~> ("Info" :: Text))
         , ("envVars" ~> envVars)
         ]
+
+showUsers :: MonadSnap m => ConnectInfo -> m ()
+showUsers connInfo = do
+    userNames <- liftIO $ withDB connInfo getUserNames
+    renderTemplate "views/_users.html" $ dict
+        [ ("userNames" ~> userNames)
+        ]
+
+getUserNames :: Connection -> IO [String]
+getUserNames conn = query_ conn "SELECT user_name FROM users" >>= pure . map fromOnly
